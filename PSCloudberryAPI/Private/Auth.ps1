0ea305cafd85ff7030e98290dec5e12bd54c107b
techinaflash <matthew@techinaflash.net>
2019-07-31 13:47:24 -0400

    Added exception message to Auth
    !deploy

--- PSCloudberryAPI/Private/Auth.ps1
+++ PSCloudberryAPI/Private/Auth.ps1
@@ -22,6 +22,8 @@ function Get-CloudberryAccessToken {
 		} catch {
 			# Dig into the exception to get the Response details.
 			# Note that value__ is not a typo.
+			Write-Host $_.Exception.Message
+			Write-Host $_.Exception.ItemName
 			Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
 			Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
 		}  
